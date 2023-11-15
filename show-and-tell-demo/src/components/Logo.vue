d1191f03039ab666d993e1e54a5522e4c4a2c560
ChrisLam <fdq09eca@gmail.com>
2023-11-15 12:58:03 +0000

    uom favicon

--- show-and-tell-demo/src/components/Logo.vue
+++ show-and-tell-demo/src/components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <img
-      src="https://assets.manchester.ac.uk/logos/hi-res/TAB_UNI_MAIN_logo/Coloured_backgrounds/TAB_col_background.png"
+      src="https://assets.manchester.ac.uk/logos/hi-res/TAB_UNI_MAIN_logo/White_backgrounds/TAB_col_white_background.png"
       alt="logo"
       width="100"
     />
